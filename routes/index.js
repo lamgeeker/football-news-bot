161d1d625bf6a03ad3f7837d0aef9da8af02262d
Maxym <161308928+lamgeeker@users.noreply.github.com>
2025-04-09 17:58:59 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,41 @@
-const express = require('express');
-const path = require('path');
-const router = express.Router();
+const functions = require('firebase-functions');
+const axios = require('axios');
 
-// Serve the index.html file for the root route
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/index.html'));
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((req, res) => {
+  const intent = req.body.queryResult.intent.displayName;
+
+  if (intent === 'GetLatestFootballNews') {
+    const apiKey = '5396ff8daa0c49fa97e42c63b16f0712';
+
+    axios.get(`https://newsapi.org/v2/everything?q=football&language=en&sortBy=publishedAt&pageSize=5&apiKey=${apiKey}`)
+      .then(response => {
+        const articles = response.data.articles;
+
+        if (articles.length === 0) {
+          res.json({ fulfillmentText: 'Не вдалося знайти новини про футбол 😕' });
+        } else {
+          const newsList = articles.map(a => `📰 *${a.title}*\n${a.url}`).join('\n\n');
+          res.json({
+            fulfillmentMessages: [
+              {
+                text: {
+                  text: ["Ось останні футбольні новини:"]
+                }
+              },
+              {
+                text: {
+                  text: [newsList]
+                }
+              }
+            ]
+          });
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        res.json({ fulfillmentText: 'Сталася помилка при отриманні новин 😓' });
+      });
+  } else {
+    res.json({ fulfillmentText: 'Інтенція не підтримується цим webhook' });
+  }
 });
-
-module.exports = router;
